eb756a4e8985e41da14de4efe87a8c1047551c88
Piotr Skrzynski <skrzynski.piotrek@gmail.com>
2020-08-29 11:05:19 +0200

    initial commit

---
+++ src/main/java/com/project/endingproject/EndingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.endingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndingprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/endingproject/EndingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.endingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
